73fce84dd9995b953d16e045fd960bdef6c5ebc1
Willian Oliveira <willian.oliveira@arizona.global>
2022-03-04 13:07:04 -0300

    add axios / realiza busca

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,16 +1,35 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
+import axios from 'axios';
 import '../styles/Search.css';
 
 const Search = () => {
+  const [value, setvalue] = useState("");
+  const [message, setMessage] = useState("Digite o registro da aeronave");
+
+  const getValue= () => {
+    console.log(value.toUpperCase());
+
+    axios('https://altinodantas.github.io/checkmark/data/dados.json')
+    .then(response => {
+        const reg = response.data.filter(item => item.MARCA === value.toUpperCase());
+        console.log(reg);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+
+    setvalue("");
+  }
+ 
   return (
     <Fragment>
       <header>
           <h1>Registro de aeronaves</h1>
       </header>
       <main>
-        <p className="Message">Digite o registro da aeronave</p>
-        <input type="text" id="registration" name="registration" className="Registration" maxLength='5'></input>
-        <button className="Btn-search">Buscar</button>
+        <p className="Message">{message}</p>
+        <input onChange={e => setvalue(e.target.value)} type="text" id="registration" name="registration" className="Registration" maxLength='5' value={value} autoFocus={true}></input>
+        <button className="Btn-search" onClick={getValue}>Buscar</button>
       </main>
     </Fragment>
   );
