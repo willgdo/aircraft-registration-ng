3dc229e73a1e3b2d0383884cbefce6840ca35009
Willian Oliveira <willian.oliveira@arizona.global>
2022-03-10 23:23:09 -0300

    Add proxy para busca

--- src/components/Search.js
+++ src/components/Search.js
@@ -18,20 +18,14 @@ const Search = ({ setProps }) => {
   }
 
   const searchRegistration = () => {
-    axios('https://altinodantas.github.io/checkmark/data/dados.json')
-    // axios('https://sistemas.anac.gov.br/dadosabertos/Aeronaves/RAB/dados_aeronaves.json', {
-    //   headers: {
-    //     'Access-Control-Allow-Origin': '*',
-    //     'Content-Type': 'application/json',
-    //   },
-    // })
-    .then(response => {
+    axios.get('/dadosabertos/Aeronaves/RAB/dados_aeronaves.json')
+      .then(response => {
         setLoading(false);
         checkRegistration(response.data)
-    })
-    .catch((err) => {
-      console.error(err);
-    });
+      })
+      .catch((err) => {
+        console.error(err);
+      });
   }
 
   const checkRegistration = (data) => {
@@ -50,23 +44,23 @@ const Search = ({ setProps }) => {
       setMessage(setLettersMessage);
     }
   }
-  
+
   return (
     <Fragment>
       <header>
         <h1>Registro de aeronaves <span className="Icon-Plane">✈️</span></h1>
       </header>
       <main>
-      <p className={`Message ${message !== defaultMessage ? 'Error' : '' }`}>{message}</p>
-      <input onChange={e => setValue(e.target.value)} type="text" id="registration" name="registration" className="Registration" maxLength='5' value={value} autoFocus={true}></input>
-      {loading ? (
-        <span className="Loader"></span> 
-      ) : (
-        <button className="Btn-Search" onClick={initSearch}>
-        <img className="Loader-Radar" src="https://c.tenor.com/8Juj0k-1L4AAAAAd/radar-love-search.gif" alt="loading" width="35px" height="35px"/>
-          Buscar
-        </button>
-      )}
+        <p className={`Message ${message !== defaultMessage ? 'Error' : ''}`}>{message}</p>
+        <input onChange={e => setValue(e.target.value)} type="text" id="registration" name="registration" className="Registration" maxLength='5' value={value} autoFocus={true}></input>
+        {loading ? (
+          <span className="Loader"></span>
+        ) : (
+          <button className="Btn-Search" onClick={initSearch}>
+            <img className="Loader-Radar" src="https://c.tenor.com/8Juj0k-1L4AAAAAd/radar-love-search.gif" alt="loading" width="35px" height="35px" />
+            Buscar
+          </button>
+        )}
       </main>
     </Fragment>
   );
